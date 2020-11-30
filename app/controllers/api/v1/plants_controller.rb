74b17e624b05462cbba9cd32878ddca8df0380a6
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-30 12:36:00 -0500

    added create plant method

--- app/controllers/api/v1/plants_controller.rb
+++ app/controllers/api/v1/plants_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::PlantsController < ApplicationController
 
+  def create
+    @plant = Plant.create(plant_params)
+    if @plant.valid?
+      render json: { plant: PlantSerializer.new(@plant) }, status: :created
+    else
+      render json: { error: 'failed to create plant' }, status: :not_acceptable
+    end
+  end
+
   def search
     @query = params[:q]
     @link = params[:link]
@@ -13,6 +22,10 @@ class Api::V1::PlantsController < ApplicationController
     # if query is bad or not present return different status
   end
 
-  
+  private
+
+  def plant_params
+    params.require(:plant).permit(:api_id)
+  end
 
 end
