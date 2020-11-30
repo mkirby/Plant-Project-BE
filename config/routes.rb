d23da3d4504c7f6ce93ffd33353714455e4a8154
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-30 12:37:30 -0500

    updated plant routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create]
       resources :user_plants, only: [:create]
+      resources :plants, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
       get '/search', to: 'plants#search'
