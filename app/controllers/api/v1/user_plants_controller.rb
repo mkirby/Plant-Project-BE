f8c287e4d79b44973a1ba9d023d8a8487f222b89
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-30 12:30:51 -0500

    added create user_plant method

--- app/controllers/api/v1/user_plants_controller.rb
+++ app/controllers/api/v1/user_plants_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::UserPlantsController < ApplicationController
+
+  def create
+    @user_plant = UserPlant.create(user_plant_params)
+    if @user_plant.valid?
+      render json: { user_plant: UserPlantSerializer.new(@user_plant) }, status: :created
+    else
+      render json: { error: 'failed to create user_plant' }, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_plant_params
+    params.require(:user_plant).permit(:user_id, :plant_id, :nickname)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      resources :user_plants, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
       get '/search', to: 'plants#search'
