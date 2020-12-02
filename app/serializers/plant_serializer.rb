59b8b85769bfd20936bf7f949ad0bb82f80410bd
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-02 10:41:40 -0500

    added an optional api data seed into the plant serializer

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,3 +1,8 @@
 class PlantSerializer < ActiveModel::Serializer
-  attributes :id, :api_id, :slug
+  attributes :id, :api_id, :slug#, :api
+
+  # can use if we need to return api data with each plant
+  def api
+    Plant.find_plant_by_slug(self.object.slug)
+  end
 end
